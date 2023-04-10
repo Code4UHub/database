ca7dde166309e907657b71442917890d816345a5
JAngelGC <ja_gonzalez42@hotmail.com>
2023-04-10 15:10:25 -0600

    Updating create.sh

---
+++ dml/users.sql
@@ -0,0 +1,35 @@
+-- students
+INSERT INTO student (student_id, first_name, last_name, email, password)
+VALUES ('A00000001', 'John', 'Smith', 'a00000001@tec.com', 'Abc123456');
+
+INSERT INTO student (student_id, first_name, last_name, email, password)
+VALUES ('A00000002', 'Emma', 'Johnson', 'a00000002@tec.com', 'Def789012');
+
+INSERT INTO student (student_id, first_name, last_name, email, password)
+VALUES ('A00000003', 'Michael', 'Brown', 'a00000003@tec.com', 'Ghi456789');
+
+INSERT INTO student (student_id, first_name, last_name, email, password)
+VALUES ('A00000004', 'Sophia', 'Lee', 'a00000004@tec.com', 'Jkl123456');
+
+INSERT INTO student (student_id, first_name, last_name, email, password)
+VALUES ('A00000005', 'Daniel', 'Wang', 'a00000005@tec.com', 'Mno789012');
+
+-- teachers
+INSERT INTO teacher (teacher_id, first_name, last_name, email, password)
+VALUES ('L00000001', 'Emily', 'Johnson', 'l00000001@tec.com', 'Abc123456');
+
+INSERT INTO teacher (teacher_id, first_name, last_name, email, password)
+VALUES ('L00000002', 'Matthew', 'Smith', 'l00000002@tec.com', 'Def789012');
+
+INSERT INTO teacher (teacher_id, first_name, last_name, email, password)
+VALUES ('L00000003', 'Olivia', 'Brown', 'l00000003@tec.com', 'Ghi456789');
+
+INSERT INTO teacher (teacher_id, first_name, last_name, email, password)
+VALUES ('L00000004', 'William', 'Lee', 'l00000004@tec.com', 'Jkl123456');
+
+INSERT INTO teacher (teacher_id, first_name, last_name, email, password)
+VALUES ('L00000005', 'Grace', 'Wang', 'l00000005@tec.com', 'Mno789012');
+
+-- administrator
+INSERT INTO administrator (first_name, last_name, email, password)
+VALUES ('Emily', 'Johnson', 'n00000001@tec.com', 'Abc123456');
