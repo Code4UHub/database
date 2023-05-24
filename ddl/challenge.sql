81c2218042659dadef3519278f1471ee103fcf55
JAngelGC <ja_gonzalez42@hotmail.com>
2023-05-24 17:24:51 -0600

    Update challenge

--- ddl/challenge.sql
+++ ddl/challenge.sql
@@ -1,7 +1,8 @@
-DROP TABLE IF EXISTS difficulty_id;
+DROP TABLE IF EXISTS difficulty;
 CREATE TABLE difficulty (
 	difficulty_id INTEGER,
     difficulty VARCHAR(15),
+    points_per_questions INTEGER,
     PRIMARY KEY (difficulty_id)
 );
 
@@ -11,6 +12,9 @@ CREATE TABLE challenge (
     module_id INTEGER, 
     difficulty_id INTEGER,
     title VARCHAR(80),
+    open_questions INTEGER,
+    closed_questions INTEGER,
+    total_points INTEGER,
     PRIMARY KEY (challenge_id),
     FOREIGN KEY (module_id) REFERENCES module(module_id),
     FOREIGN KEY (difficulty_id) REFERENCES difficulty(difficulty_id)
@@ -23,5 +27,28 @@ CREATE TABLE question (
     type VARCHAR(10),
     challenge_id INTEGER,
     PRIMARY KEY (question_id),
-    FOREIGN KEY (challenge_id) REFERENCES challenge(challenge_id),
+    FOREIGN KEY (challenge_id) REFERENCES challenge(challenge_id)
 );
+
+DROP TABLE IF EXISTS student_challenge;
+CREATE TABLE student_challenge (
+    student_id VARCHAR(9),
+	challenge_id INTEGER,
+    score INTEGER,
+    PRIMARY KEY (challenge_id, student_id),
+    FOREIGN KEY (student_id) REFERENCES student(student_id),
+    FOREIGN KEY (challenge_id) REFERENCES challenge(challenge_id)
+);
+
+
+DROP TABLE IF EXISTS student_question;
+CREATE TABLE student_question (
+    student_id VARCHAR(9),
+	question_id INTEGER,
+    solution JSONB,
+    passed BOOLEAN,
+    PRIMARY KEY (student_id, question_id),
+    FOREIGN KEY (student_id) REFERENCES student(student_id),
+    FOREIGN KEY (question_id) REFERENCES question(question_id)
+);
+
--- dml/challenge.sql
+++ dml/challenge.sql
@@ -1,5 +1,3 @@
-INSERT INTO difficulty (difficulty_id, difficulty) VALUES (1, 'Fácil');
-
-INSERT INTO difficulty (difficulty_id, difficulty) VALUES (2, 'Medio');
-
-INSERT INTO difficulty (difficulty_id, difficulty) VALUES (3, 'Difícil');
+INSERT INTO difficulty (difficulty_id, difficulty, points_per_questions) VALUES (1, 'Fácil', 30);
+INSERT INTO difficulty (difficulty_id, difficulty, points_per_questions) VALUES (2, 'Medio', 50);
+INSERT INTO difficulty (difficulty_id, difficulty, points_per_questions) VALUES (3, 'Difícil', 70);
