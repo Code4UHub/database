c4c5715e6d3ab1fdb838b689cb36983966824a9c
diaz <carlosd2mil@gmail.com>
2023-06-13 20:39:55 -0600

    Run triggers and dml in order so it all works

--- setup.sh
+++ setup.sh
@@ -10,14 +10,14 @@ source ./scripts/env.sh
 echo -e "\nRunning Data Definition Language"
 ./scripts/import.sh ddl
 
+# # Create function
+echo -e "\nCreating database functions"
+./scripts/import.sh function
+#
+# # Create triggers
+echo -e "\nCreating database triggers"
+./scripts/import.sh scripts
+
 # # Run dml files to fill tables
 echo -e "\nRunning Data Manipulation Language"
 ./scripts/import.sh dml
-#
-# # Create function
-# echo -e "\nCreating database functions"
-# ./scripts/import.sh function
-#
-# # Create triggers
-# echo -e "\nCreating database triggers"
-# ./scripts/import.sh scripts
